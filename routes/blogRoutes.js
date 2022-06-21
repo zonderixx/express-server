dcb19934c2b8bc79a5ab23ab724bae7f93f10191
zonderixx <babushkod@gmail.com>
2022-06-21 21:26:56 +0300

    first commit

---
+++ routes/blogRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const blogController = require("../controllers/blogController");
+
+const router = express.Router();
+
+router.get("/blogs/create", blogController.blog_create_get);
+router.get("/blogs", blogController.blog_index);
+router.post("/blogs", blogController.blog_create_post);
+router.get("/blogs/:id", blogController.blog_details);
+router.delete("/blogs/:id", blogController.blog_delete);
+
+module.exports = router;
